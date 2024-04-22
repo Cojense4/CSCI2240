4ce22f547b82281c3c072f4cfa9b8511955da0d3
Cojense4 <cojense4@gmail.com>
2024-04-22 16:36:09 -0500

    V1.0

---
+++ Prog01/hello.c
@@ -0,0 +1,15 @@
+/*
+* Auth: Connor Jensen
+* Date: 02-01-24 (Due: 02-11-24)
+* Course: CSCI-2240 (Sec: 1241)
+* Desc: PREPROJECT-01, A Simple "Hello, World" C program.
+*/
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+int main( void ) {
+/* Personalized hello. */
+printf( "Hello, World, from Connor Jensen!\n" );
+/* Proper way to finish a program in a POSIX-system. */
+exit( EXIT_SUCCESS );
+} /* end main() */
